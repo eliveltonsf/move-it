d15bdc9dce9ac7b9dd8ec28c789cf3dc8364c3e7
Elivelton Ferreira <elivelton.seven@gmail.com>
2022-12-16 19:05:19 -0300

    :art: add css module for main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import ExperienceBar from "../components/ExperienceBar";
+import styles from '../styles/main.module.css';
 export default function Home() {
   return (
-    <div className='container'>
+    <div className={styles.container}>
       <ExperienceBar />
     </div>
   ) 
