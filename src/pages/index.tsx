140216e65781f423c0565a18d1627eae08256d64
Elivelton Ferreira <elivelton.seven@gmail.com>
2022-12-16 19:20:41 -0300

    :hammer: applying eslint in code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import ExperienceBar from "../components/ExperienceBar";
+import ExperienceBar from '../components/ExperienceBar';
 import styles from '../styles/main.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
       <ExperienceBar />
     </div>
-  ) 
+  );
 }
